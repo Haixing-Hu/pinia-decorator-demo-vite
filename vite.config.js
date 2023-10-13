69a6570ca487590ebe50ba907e554bfae56da6b1
Haixing Hu <starfish.hu@gmail.com>
2023-10-13 15:38:43 +0800

    feat: finish the demo

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ *
+ *    Copyright (c) 2022 - 2023.
+ *    Haixing Hu, Qubit Co. Ltd.
+ *
+ *    All rights reserved.
+ *
+ ******************************************************************************/
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import babel from '@haixing_hu/vite-plugin-babel';
+
+export default defineConfig({
+  base: './',                               // use relative pathes for all resources
+  plugins: [
+    vue({
+      script: {
+        babelParserPlugins: ['decorators'], // enable decorators support
+      },
+    }),
+    babel(),                                // must after the vue plugin
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
